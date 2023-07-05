34a6b8b4ecf6f6345aba3a0117e26e22798ebc1d
Vikas Sanodiya <103686747+vikas-011@users.noreply.github.com>
2023-07-05 12:31:51 +0530

    number change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,47 @@
 
-
 function SendEmail(){
     event.preventDefault()
-    var parmas ={
-        from_name : document.getElementById("fullName").value,
-        email_id : document.getElementById("email_id").value,
-        phone_id : document.getElementById("phone_id").value,
-       
-        message : document.getElementById("Message").value
+    var fullName = document.getElementById("fullName").value;
+    var email_id = document.getElementById("email_id").value;
+    var phone_id = document.getElementById("phone_id").value;
+    var message = document.getElementById("Message").value;
+    
+    if (fullName === "" || email_id === "" || message === "") {
+      // Display  an error message if any of the fields are empty
+      swal("⌛PLEASE ENTER⏳\n ❓NAME‍\n ❓EMAIL\n ❓MESSAGE");
+      return;
     }
+    
+
+    // var parmas ={
+    //     from_name : document.getElementById("fullName").value,
+    //     email_id : document.getElementById("email_id").value,
+    //     phone_id : document.getElementById("phone_id").value,
+       
+    //     message : document.getElementById("Message").value
+
+       
+    // }
+    var parmas = {
+        from_name: fullName,
+        email_id: email_id,
+        phone_id: phone_id,
+        message: message
+      }
+
     empty();
     emailjs.send("service_77v0y07", "template_1jqqn6o", parmas).then(function(res){
-        swal(" Message sent successfully ");
+        swal("✔ Message sent successfully\n \n⏰  CHECK YOUR EMAIL  📧");
        
         
         // console.log(parmas)
     })
     console.log(parmas)
+   
 }
 
-
-
 function empty(){
+    
 
     setTimeout(()=>{document.getElementById("fullName").value=null
     document.getElementById("email_id").value=null
@@ -29,7 +49,7 @@ function empty(){
     document.getElementById("Message").value=null
    
     
-},10000);
+},7000);
     
 
 }
